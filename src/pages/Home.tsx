39bcb2fa7d6607e6347756436fd2faa639ec4f66
Shivashankar L <ss.lakshminarayan@myeduate.com>
2022-01-20 12:11:51 +0530

    Added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useLazyQuery } from "@apollo/client";
+import { GET_WEATHER_QUERY } from "../graphql/Queries"
+
+function Home()
+{
+    const [city, setCity] = useState(" "); 
+    const [getWeather, { loading, data, error}] = useLazyQuery(
+        GET_WEATHER_QUERY,
+        {
+            variables: {name: city},
+        }
+    );
+
+    if (error) return <h1>Error Found</h1>
+            
+    if(data) {
+        console.log(data);
+
+    }
+    return(
+        <div className='Home'>
+            <h1>Enter the city</h1>
+            <input type="text" placeholder='City Name' onChange={(event) => setCity(event.target.value)} /> 
+            <button onClick={()=>getWeather}>
+                Find..
+            </button>
+
+            <div className='weather'>
+                {data && (
+                    <>
+                        <h1>{data.getCityByName.name}</h1>
+                        <h1>{data.getCityByName.weather.temperature.actual}</h1>
+                    
+                    
+                    </>
+                )}
+            </div>
+
+        </div>
+    )
+
+
+}
+
+export default Home;
